39d8a4954e27b53e494cb9f96e3ea5528d12bbab
David Ochiel <nguonodave@gmail.com>
2025-04-04 22:22:03 +0300

    control flow (loop)

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -9,6 +9,18 @@ fn main() {
 
     // using if else in a variable
     let high = true;
-    let num = if high {9} else {7};
-    println!("{}", num)
+    let num = if high { 9 } else { 7 };
+    println!("{}", num);
+
+    // loops
+    let mut c = 1;
+    let res = loop {
+        c += 1;
+        println!("wow");
+        if c == 10 {
+            break c;
+        }
+    };
+
+    println!("{}", res)
 }
