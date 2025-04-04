e1a6a6c294e06a88f3bbd9b0e88901c4bbfad932
David Ochiel <nguonodave@gmail.com>
2025-04-04 16:27:22 +0300

    feat: looping guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,29 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("This is a guessing game!");
+    println!("Guess a number between 1-100");
+
+    let secret = rand::rng().random_range(1..=101);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("msg");
+
+        let guess: u32 = guess.trim().parse().expect("enter number");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("less"),
+            Ordering::Greater => println!("greater"),
+            Ordering::Equal => {
+                println!("equal");
+                break;
+            }
+        }
+    }
 }
