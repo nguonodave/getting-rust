b9bd27d36e22cec09ef2e90440da860ce2151abc
David Ochiel <nguonodave@gmail.com>
2025-04-05 14:46:46 +0300

    control flow (for loop)

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -31,5 +31,22 @@ fn main() {
         println!("the dig is {}", dig);
     }
 
-    println!("done")
+    println!("done");
+
+    let ss = [10, 2, 4, 66, 44, 77, 99];
+
+    for s in ss.iter() {
+        println!("{}", s)
+    }
+
+    /*
+    ... "in ss" implicitly calls the .into_iter() function
+    since the ss is a primitive type, which is a copy, hence ownership won't ba transfered, it will not be consumed
+    if ss would have been a collection, eg a vector then it would have been consumed by the into_inter fiunction
+    */
+    for n in ss {
+        println!("{}", n)
+    }
+
+    println!("{:?}", ss)
 }
